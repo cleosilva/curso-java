a290b41a1a8bf0e052ffe2eae86c101a39523682
Cleonir Silva <82469705+cleosilva@users.noreply.github.com>
2022-05-24 20:14:10 -0300

    alterando nome projeto

--- helloJava/src/helloJava/HelloJava.java
+++ helloJava/src/helloJava/HelloJava.java
@@ -4,6 +4,7 @@ public class HelloJava {
 	public static void main(String[] args) {
 		System.out.println("Hello Java World!");
 		System.out.println("Testando commit git");
+		System.out.println("Testando mudança de nome");
 	}
 
 }
