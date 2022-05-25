e0fbca74077ff89cd672e10e103c218c3ac712ea
Cleonir Silva <82469705+cleosilva@users.noreply.github.com>
2022-05-25 09:58:03 -0300

    escopo de variáveis

---
+++ helloJava/src/cursoJava/Variavel.java
@@ -0,0 +1,21 @@
+package cursoJava;
+
+public class Variavel {
+	// Declarando variável global acessível a todos
+	static int maiorIdadeGlobal = 30;
+		
+	public static void main(String[] args) {
+		// Declarando variável local acessível somente ao método
+		int maiorIdade = 18;
+		System.out.println("Valor da variável loca = " + maiorIdade);
+			
+		// Para executar o método deve chamá-lo dentro do main
+		getVariavel();
+	}
+	
+	public static void getVariavel() {
+		System.out.println("Valor da variável global = " + maiorIdadeGlobal);
+		//System.out.println(maiorIdade); // Erro não consegue acessar a variável local
+	}
+
+}
