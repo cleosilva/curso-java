3d37ef31025428b07df9ff58fac12fad9f0bc678
Cleonir Silva <82469705+cleosilva@users.noreply.github.com>
2022-05-29 21:40:59 -0300

    subindo novamente as constantes

--- helloJava/src/cursoJava/constantes/StatusAluno.java
+++ helloJava/src/cursoJava/constantes/StatusAluno.java
@@ -1,5 +1,8 @@
 package cursoJava.constantes;
 
 public class StatusAluno {
-
+	// static significa que é uma constante. O valor nao vai mudar no sistema
+	public static String APROVADO = "Aprovado";
+	public static String RECUPERACAO = "Recuperação";
+	public static String REPROVADO = "Reprovado";
 }
--- helloJava/src/orientacaoObjetos/Aluno.java
+++ helloJava/src/orientacaoObjetos/Aluno.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+import cursoJava.constantes.StatusAluno;
+
 public class Aluno {
 	
 		// Esses são os atributos do aluno
@@ -133,12 +135,12 @@ public class Aluno {
 			double media = this.getMediaNota();
 			if (media >= 50) {
 				if (media >= 70) {					
-					return "Aluno aprovado";
+					return StatusAluno.APROVADO;
 				} else {
-					return "Aluno em recuperação";
+					return StatusAluno.RECUPERACAO;
 				}
 			} else {
-				return "Aluno reprovado";
+				return StatusAluno.REPROVADO;
 			}
 		}
 
